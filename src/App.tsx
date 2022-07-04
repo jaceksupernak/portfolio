e67da93b4a1828aa12a02d43d4820e53505f7325
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-04 12:44:05 +0100

    remove dummy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import React from 'react';
-import './App.css';
-import Button from './components/button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button text="siema Eeeniu!" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App:React.FC = () => (
+  <> </>
+);
 
 export default App;
