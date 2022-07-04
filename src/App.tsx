d325617a2e4ac46dfcb64bd1480143d55621447b
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-04 10:49:15 +0100

    initial create-react-app install and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button text="siema Eeeniu!" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
