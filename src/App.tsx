bc76bd4a1d54a63a928116ea677bda789f26d00a
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-07 08:31:58 +0100

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './theme/globalStyle';
 
 const App:React.FC = () => (
-  <> </>
+  <>
+    <GlobalStyle />
+    test
+  </>
 );
 
 export default App;
