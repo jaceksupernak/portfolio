cf4dd1fcda0fec5fa0072b47209ea30d05045c1d
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-10 08:44:02 +0100

    create Socials component

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ProfilePicture from '../profilepicture/ProfilePicture';
+import Socials from '../socials/Socials';
 
 const ProfileStyled = styled.section`
     display: flex;
@@ -13,7 +14,7 @@ const Profile:React.FC = () => (
   <ProfileStyled>
 
     <ProfilePicture />
-    <div>git in twtr</div>
+    <Socials />
 
   </ProfileStyled>
 
---
+++ src/components/socials/Socials.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SocialsStyled = styled.section`
+
+`;
+
+const Socials:React.FC = () => (
+  <SocialsStyled>
+
+    sd asd asd
+
+  </SocialsStyled>
+
+);
+
+export default Socials;
