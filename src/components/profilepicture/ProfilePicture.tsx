22c248edda4a1501e4d8d8564ed735fc7f3c9d3b
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-10 08:36:46 +0100

    style ProfilePicture component

--- src/components/profilepicture/ProfilePicture.tsx
+++ src/components/profilepicture/ProfilePicture.tsx
@@ -3,11 +3,26 @@ import styled from 'styled-components';
 import myFace from '../../assets/profile_picture.png';
 
 const ProfilePictureStyled = styled.img`
+  border-radius: 40px;
+  height: 200px;
+  width: 200px;
 
+
+  @media (min-width: ${({ theme }) => theme.screen.tablet}) {
+    height: 240px;
+    width: 240px;
+    margin-top: 40px;
+  }
+
+  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+    height: 260px;
+    width: 260px;
+    margin-top: 80px;
+  }
 `;
 
 const ProfilePicture:React.FC = () => (
-  <ProfilePictureStyled src={myFace} />
+  <ProfilePictureStyled src={myFace} alt="Picture of my face" />
 
 );
 
