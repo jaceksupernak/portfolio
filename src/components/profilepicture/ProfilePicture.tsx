9ce012a777e40fbafe7d75d996d073df1bca77c4
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-10 08:15:30 +0100

    declare *.png module in index.d.tsx

--- src/components/profilepicture/ProfilePicture.tsx
+++ src/components/profilepicture/ProfilePicture.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import myFace from '../../../assets/profile_picture.png';
+import myFace from '../../assets/profile_picture.png';
 
 const ProfilePictureStyled = styled.img`
 
 `;
 
 const ProfilePicture:React.FC = () => (
-  <ProfilePictureStyled />
+  <ProfilePictureStyled src={myFace} />
 
 );
 
