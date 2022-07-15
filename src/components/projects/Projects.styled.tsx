8367fc8c2b4c3ec475bd90329a45dc2501f29763
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-15 15:11:03 +0100

    transfer Projects' style to Projects.styled

---
+++ src/components/projects/Projects.styled.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+export const ProjectsContainerStyled = styled.section`
+    margin-top: 10px;
+`;
+
+export const ProjectStyled = styled.div`
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    border-radius: 4px;
+    width: 100%;
+    gap: 30px;
+    background: linear-gradient(91.78deg, #515586 -44.01%, rgba(255, 255, 255, 0) 140.31%);
+    padding: 10px;
+
+    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
+        padding: 10px 20px 20px 20px;
+    }
+
+    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+        justify-content: space-between;
+        flex-direction: row;
+        height: 290px;
+        padding-bottom: 10px;
+    }
+
+`;
+
+export const ProjectIconStyled = styled.img`
+    position: absolute;
+    top: -24px;
+    right: 50%;
+    transform: translateX(50%);
+    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+        top: -24px;
+        right: -24px;
+        transform: none;
+    }
+`;
+
+export const AboutStyled = styled.div`
+    
+`;
+
+export const PresentationStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+`;
+
+export const ImgStyled = styled.img`
+    height: 139px;
+    width: 270px;
+    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
+        height: 163px;
+        width: 316px;
+    }
+`;
+
+export const ButtonsContainerStyled = styled.div`
+    display: flex;
+    gap: 3px;
+    @media (min-width: ${({ theme }) => theme.screen.mobile}) {
+        gap: 10px;
+    }
+`;
+
+export const TechnologiesContainerStyled = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 15px;
+    margin-top: 20px;
+    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+        justify-content: start; 
+    }
+`;
+
+export const TechnologyStyled = styled.div`
+    background-color: ${({ theme }) => theme.primaryDark};
+    border-radius: 13px;
+    padding: 10px;
+`;
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,96 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SectionHeadingStyled, Heading3Styled, SpanStyled } from '../../theme/styledElements';
 // import { projectsData } from '../../data/Data';
+import Button from '../button/Button';
+import {
+  ProjectsContainerStyled,
+  ProjectStyled,
+  ProjectIconStyled,
+  AboutStyled,
+  PresentationStyled,
+  ImgStyled,
+  ButtonsContainerStyled,
+  TechnologiesContainerStyled,
+  TechnologyStyled,
+} from './Projects.styled';
+
 import chinguLogoSmall from '../../assets/experience_chingu_small.png';
 import projectLiteTalk from '../../assets/project_litetalk.png';
-import Button from '../button/Button';
-
-const ProjectsContainerStyled = styled.section`
-    margin-top: 10px;
-`;
-
-const ProjectStyled = styled.div`
-    display: flex;
-    position: relative;
-    flex-direction: column;
-    border-radius: 4px;
-    width: 100%;
-    gap: 30px;
-    background: linear-gradient(91.78deg, #515586 -44.01%, rgba(255, 255, 255, 0) 140.31%);
-    padding: 10px;
-
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        padding: 10px 20px 20px 20px;
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        justify-content: space-between;
-        flex-direction: row;
-        height: 290px;
-        padding-bottom: 10px;
-    }
-
-`;
-
-const ProjectIconStyled = styled.img`
-    position: absolute;
-    top: -24px;
-    right: 50%;
-    transform: translateX(50%);
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        top: -24px;
-        right: -24px;
-        transform: none;
-    }
-`;
-
-const AboutStyled = styled.div`
-    
-`;
-
-const PresentationStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 15px;
-`;
-
-const ImgStyled = styled.img`
-    height: 139px;
-    width: 270px;
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        height: 163px;
-        width: 316px;
-    }
-    
-`;
-
-const ButtonsContainerStyled = styled.div`
-    display: flex;
-    gap: 3px;
-    @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-        gap: 10px;
-    }
-`;
-
-const TechnologiesContainerStyled = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 15px;
-    margin-top: 20px;
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        justify-content: start; 
-    }
-`;
-
-const TechnologyStyled = styled.div`
-    background-color: ${({ theme }) => theme.primaryDark};
-    border-radius: 13px;
-    padding: 10px;
-`;
 
 const Projects:React.FC = () => (
   <>
