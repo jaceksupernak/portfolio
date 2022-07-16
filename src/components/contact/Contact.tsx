d27b276b84c2064bbe23287556e7917981435f37
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 19:58:10 +0100

    Create Contact.styled and transfer Contact styled components over there

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,112 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import Button from '../button/Button';
 import wavyLine from '../../assets/wavy_line.png';
 import emailIcon from '../../assets/email_icon.png';
-
-const ContactContainerStyled = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border-radius: 34px;
-    margin-top: 50px;
-    padding: 30px 0px;
-    gap: 30px;
-    background: ${({ theme }) => `linear-gradient(98.09deg, ${theme.primaryDarkBrighter} -31.03%, ${theme.secondaryLightBrighter} 79.54%)`};
-
-    @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-        padding: 50px 0px;
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        padding: 50px 0px;
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        padding: 70px 0px;
-    }
-`;
-
-const ContactTextStyled = styled.span`
-    position: relative;
-    font-weight: bold;
-    font-size: 1.8rem;
-    color: ${({ theme }) => theme.primaryDark};
-    z-index: 1;
-
-    @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-        font-size: 2rem;
-    }
-    
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        font-size: 3rem;
-    }
-`;
-
-const WavyLineStyled = styled.img`
-    position: absolute;
-    bottom: -28%;
-    right: 50%;
-    transform: translateX(50%);
-    max-width: 80vw; 
-
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        max-width: 90vw; 
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        max-width: 100vh;
-    }
-`;
-
-const ButtonStyleWrapper = styled.div`
-    a {
-        font-size: 1.2rem;
-        max-width: 85vw;
-        height: 50px;
-    }
-    a img {
-        display: none;
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-        a {
-            font-size: 1.5rem;
-            height:70px;
-        }
-    }
-
-    @media (min-width: 400px) {
-        a {
-            height: 90px;
-        }
-        a img {
-        display: block;
-        height: 50px;
-        width: 50px;
-        }
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-        a {
-            width: 400px;
-        }
-    }
-
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        a {
-            font-size: 2rem;
-            width:480px;
-            height:115px;
-        }
-        a img {
-            height: 70px;
-            width: 70px;
-        }
-    }
-    
-`;
+import {
+  ContactContainerStyled,
+  ContactTextStyled,
+  WavyLineStyled,
+  ButtonStyleWrapper,
+} from './Contact.styled';
 
 const Contact:React.FC = () => (
   <ContactContainerStyled>
