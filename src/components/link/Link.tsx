5377e2f336d79ee76e9d4be202dba401fb233f2c
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:25:21 +0100

    create Link.styled and transfer the Link styles there

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import LinkStyled from './Link.styled';
 
 interface LinkProps {
     text: string;
     href: string;
 }
-
-const LinkStyled = styled.a`
-    text-decoration: none;
-    transition: color 0.3s linear;
-    &:hover {
-      color: ${({ theme }) => theme.secondaryLightBrighter};
-    }
-`;
 
 const Link:React.FC<LinkProps> = ({ text, href }) => (
   <LinkStyled href={href}>
