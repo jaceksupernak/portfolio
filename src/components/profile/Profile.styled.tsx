d7ea08aa7b2a6d67c2c44a2268d49249ceaca3eb
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:32:01 +0100

    create Profile.styled and transfer the Profile styles there

---
+++ src/components/profile/Profile.styled.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ProfileStyled = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export default ProfileStyled;
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import ProfilePicture from '../profilepicture/ProfilePicture';
 import Socials from '../socials/Socials';
-
-const ProfileStyled = styled.section`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`;
+import ProfileStyled from './Profile.styled';
 
 const Profile:React.FC = () => (
   <ProfileStyled>
