ba8feb8fe4eb509743c8b587607b60515cbc45ad
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:41:21 +0100

    create Navbar.styled and transfer the Navbar styles there

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import Logo from '../logo/Logo';
 import Navbarlinks from '../navbarlinks/Navbarlinks';
 import MobileMenu from '../mobilemenu/MobileMenu';
-
-const NavbarStyled = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  font-weight: 100;
-  font-size: 1rem;
-
-  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-    font-size: 1.3rem;
-    }
-`;
+import NavbarStyled from './Navbar.styled';
 
 const Navbar:React.FC = () => (
   <NavbarStyled>
