e23dcc45312fd530e487ddd72c5cb1a8d34635c4
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:33:42 +0100

    create ProfilePicture.styled and transfer the ProfilePicture styles there

--- src/components/profilepicture/ProfilePicture.tsx
+++ src/components/profilepicture/ProfilePicture.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import ProfilePictureStyled from './ProfilePicture.styled';
 import myFace from '../../assets/profile_picture.png';
-
-const ProfilePictureStyled = styled.img`
-  border-radius: 40px;
-  height: 200px;
-  width: 200px;
-
-
-  @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-    height: 240px;
-    width: 240px;
-  }
-
-  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-    height: 260px;
-    width: 260px;
-  }
-`;
 
 const ProfilePicture:React.FC = () => (
   <ProfilePictureStyled src={myFace} alt="Picture of my face" />
