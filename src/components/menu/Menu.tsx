a36fca0d694bd38ab7bfc81919c248e2558f6b5c
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:39:55 +0100

    create Menu.styled and transfer the Menu styles there

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,39 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import Link from '../link/Link';
-
-interface MenuStyledProps {
-    isOpen: boolean;
-}
-
-const MenuStyled = styled.div<MenuStyledProps>`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: ${({ theme }) => theme.primaryLight};
-  height: 100vh;
-  text-align: center;
-  text-transform: uppercase;
-  position: fixed;
-  font-weight: bold;
-  top: 0;
-  right: 0;
-  width: 100%;
-  transition: transform 0.3s ease-in-out;
-  transform: ${({ isOpen }) => (isOpen ? 'translateX(0)' : 'translateX(100%)')};
-  
-  a {
-    font-size: 2rem;
-    padding: 1.5rem 0;
-    letter-spacing: 0.2rem;
-    color: ${({ theme }) => theme.primaryDark};
-  }
-
-  @media (min-width: ${({ theme }) => theme.screen.mobile}) {
-    max-width: 320px;;
-  }
-    
-`;
+import MenuStyled from './Menu.styled';
 
 interface MenuProps {
     isOpen: boolean;
