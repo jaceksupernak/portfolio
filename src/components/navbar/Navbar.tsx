267397ae33fb7107a850b55e55a23817ac010fe3
Jacek Supernak <66032213+jaceksupernak@users.noreply.github.com>
2022-07-16 09:48:25 +0100

    create NavbarLinks.styled and transfer the NavbarLinks styles there

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Logo from '../logo/Logo';
-import Navbarlinks from '../navbarlinks/Navbarlinks';
+import NavbarLinks from '../navbarlinks/NavbarLinks';
 import MobileMenu from '../mobilemenu/MobileMenu';
 import NavbarStyled from './Navbar.styled';
 
 const Navbar:React.FC = () => (
   <NavbarStyled>
     <Logo />
-    <Navbarlinks />
+    <NavbarLinks />
     <MobileMenu />
   </NavbarStyled>
 );
--- src/components/navbarlinks/Navbarlinks.tsx
+++ src/components/navbarlinks/Navbarlinks.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import Link from '../link/Link';
+import NavbarLinksStyled from './NavbarLinks.styled';
 
-const NavbarlinksStyled = styled.div`
-  display: none;
-  gap: 20px;
-  
-
-  @media (min-width: ${({ theme }) => theme.screen.tablet}) {
-    display: flex;
-    }
-
-  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-    gap: 42px;
-  }
-`;
-
-const Navbarlinks:React.FC = () => (
-  <NavbarlinksStyled>
+const NavbarLinks:React.FC = () => (
+  <NavbarLinksStyled>
     <Link text="experience" href="https://www.google.com" />
     <Link text="projects" href="https://www.google.com" />
     <Link text="get in touch" href="https://www.google.com" />
     <Link text="cv" href="https://www.google.com" />
-  </NavbarlinksStyled>
+  </NavbarLinksStyled>
 );
 
-export default Navbarlinks;
+export default NavbarLinks;
