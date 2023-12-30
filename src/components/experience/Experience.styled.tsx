76aa40a4e08be11110d4507fd53176f3d30c8c4a
Jacek Supernak <jj.supernak@gmail.com>
2023-12-30 11:17:22 +0000

    align experience to left when on mobile

--- src/components/experience/Experience.styled.tsx
+++ src/components/experience/Experience.styled.tsx
@@ -1,43 +1,46 @@
 import styled from 'styled-components';
 
 export const ExperienceContainerStyled = styled.section`
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
 `;
 
 export const ExperienceStyled = styled.article`
-    display: flex;
-    justify-content: end;
-    align-items: center;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        justify-content: space-between;
-    }
+  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+    justify-content: space-between;
+  }
 `;
 
 export const ExperienceLogoLaptopViewStyled = styled.img`
-    display: none;
+  display: none;
 
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        display: block;
-    }
+  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+    display: block;
+  }
 `;
 
 export const ExperienceAboutStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: end;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+    align-items: flex-end;
+  }
 `;
 
 export const CompanyContainerStyled = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 export const CompanyLogoStyled = styled.img`
-    @media (min-width: ${({ theme }) => theme.screen.laptop}) {
-        display: none;
-    }
+  @media (min-width: ${({ theme }) => theme.screen.laptop}) {
+    display: none;
+  }
 `;
