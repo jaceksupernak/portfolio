07eda84ba219ee9e93ae75b7c171cf5aa3cb1edb
Jacek Supernak <jj.supernak@gmail.com>
2024-01-15 03:45:37 +0000

    open links in new tab

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,27 +2,35 @@ import React from 'react';
 import ButtonStyled from './Button.styled';
 
 interface iconProps {
-  src: string,
-  alt: string,
-  height: string,
+  src: string;
+  alt: string;
+  height: string;
   width: string;
   gapToText?: string;
 }
 
 interface ButtonProps {
-  type: string,
-  text: string,
-  href: string,
-  width: string,
-  height: string,
-  fontSize: string,
+  type: string;
+  text: string;
+  href: string;
+  width: string;
+  height: string;
+  fontSize: string;
   toLowerCase?: boolean;
   lightWeight?: boolean;
   icon?: iconProps;
 }
 
-const Button:React.FC<ButtonProps> = ({
-  type, text, href, width, height, fontSize, toLowerCase, lightWeight, icon,
+const Button: React.FC<ButtonProps> = ({
+  type,
+  text,
+  href,
+  width,
+  height,
+  fontSize,
+  toLowerCase,
+  lightWeight,
+  icon,
 }) => (
   <ButtonStyled
     type={type}
@@ -32,18 +40,18 @@ const Button:React.FC<ButtonProps> = ({
     toLowerCase={toLowerCase}
     lightWeight={lightWeight}
     href={href}
+    target="_blank"
+    rel="noopener noreferrer"
     gapToText={icon && icon.gapToText ? icon.gapToText : '5px'}
   >
-    {icon
-      ? (
-        <img
-          src={icon.src}
-          alt={icon.alt}
-          height={icon.height}
-          width={icon.width}
-        />
-      )
-      : null}
+    {icon ? (
+      <img
+        src={icon.src}
+        alt={icon.alt}
+        height={icon.height}
+        width={icon.width}
+      />
+    ) : null}
 
     {text}
   </ButtonStyled>
